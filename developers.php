65ed4c968f68721ee8712c1df0daa094a30b0034
Namra14 <0320-0543@lspu.edu.ph>
2023-06-11 13:48:21 +0800

    updated

---
+++ developers.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Developers</title>
+    <link rel="stylesheet" href="style.css">
+
+    <style>
+        body {
+            background: linear-gradient(45deg, #C4DFDF, #4E3636, #EEE2DE, #967E76);
+            background-size: 400% 400%;
+            animation: wavingColor 10s ease infinite;
+        }
+
+        @keyframes wavingColor {
+            0% {
+                background-position: 0% 50%;
+            }
+            50% {
+                background-position: 100% 50%;
+            }
+            100% {
+                background-position: 0% 50%;
+            }
+        }
+    </style>
+
+</head>
+<body>
+    <header>
+        <a href="index.php"><img class="logo" src="img/newl.png" width="270px" height="130px"></a>
+        <nav class="navigation">
+            <a href="index.php">Home</a>
+            <a href="index.php#about">About</a>
+            <a href="services.php">Service</a>
+            <a href="contact.php">Contact</a>
+        </nav>
+        
+    </header><br><br><br><br><br><br><br><br>
+
+    <div class="title">
+    <h1>The Developers</h1>
+    </div><br><br>
+    
+    <div class="image">
+    <img src="img/Developer.jpg" alt="Picture of the artist">
+    <img src="img/Developer.jpg" alt="Picture of the artist">
+    <img src="img/Developer.jpg" alt="Picture of the artist">
+    </div><br><br>
+
+    <div class="artist">
+    <p>James Roland L. Angeles
+        <br>3WMAD-1
+        <br>BS Information Technology Student
+        <br>21 Years Old
+        
+    </p>
+
+    <p>James Roland L. Angeles
+        <br>3WMAD-1
+        <br>BS Information Technology Student
+        <br>21 Years Old
+        
+    </p>
+    
+    <p>James Roland L. Angeles
+        <br>3WMAD-1
+        <br>BS Information Technology Student
+        <br>21 Years Old
+        
+    </p>
+    </div>><br><br><br><br><br><br>
+
+
+    <script src="script.js"></script>
+    
+</body>
+</html>
