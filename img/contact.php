5d2f99e24da498bcde903fdda0ba38558aa814ac
Namra14 <0320-0543@lspu.edu.ph>
2023-06-14 00:38:47 +0800

    forms

---
+++ img/contact.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OAIR Website</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <a href="index.php"><img class="logo" src="img/newl.png" width="270px" height="130px"></a>
+        <nav class="navigation">
+            <a href="index.php">Home</a>
+            <a href="index.php#about">About</a>
+            <a href="services.php">Service</a>
+            <a href="applicationform.php">Forms</a>
+            <a href="contact.php" class="active">Contact</a>
+        </nav>
+        
+    </header>
+
+    <section class="parallax">
+        <img src="img/hill1.png" id="hill1">
+        <img src="img/hill2.png" id="hill2">
+        <img src="img/hill3.png" id="hill3">
+        <img src="img/hill4.png" id="hill4">
+        <img src="img/hill5.png" id="hill5">
+        <img src="img/tree.png" id="tree">
+        <h2 id="text">Contact</h2>
+        <img src="img/leaf.png" id="leaf">
+        <img src="img/plant.png" id="plant">
+    </section>
+
+    <script src="script.js"></script>
+</body>
+</html>
---
+++ img/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OAIR Website</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <a href="index.html"><img class="logo" src="img/newl.png" width="270px" height="130px"></a>
+        <nav class="navigation">
+            <a href="index.php" class="active">Home</a>
+            <a href="index.php#about">About</a>
+            <a href="services.php">Service</a>
+            <a href="applicationform.php">Forms</a>
+            <a href="contact.php">Contact</a>
+        </nav>
+        
+    </header>
+
+    <section class="parallax">
+        <img src="img/hill1.png" id="hill1">
+        <img src="img/hill2.png" id="hill2">
+        <img src="img/hill3.png" id="hill3">
+        <img src="img/hill4.png" id="hill4">
+        <img src="img/hill5.png" id="hill5">
+        <img src="img/tree.png" id="tree">
+        <h2 id="text">Agricultural Website</h2>
+        <img src="img/leaf.png" id="leaf">
+        <img src="img/plant.png" id="plant">
+    </section>
+
+
+    <section class="sec" id="about">
+        <h2>Online Agricultural Insurance Registry and Management System </h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Omnis sit saepe ex minima iste maxime repellendus porro culpa quae modi, 
+            itaque reiciendis facere repellat corporis, error ad quo aperiam sint?Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Omnis sit saepe ex minima iste maxime repellendus porro culpa quae modi, 
+            itaque reiciendis facere repellat corporis, error ad quo aperiam sint?Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Omnis sit saepe ex minima iste maxime repellendus porro culpa quae modi, 
+            itaque reiciendis facere repellat corporis, error ad quo aperiam sint?<br><br>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Omnis sit saepe ex minima iste maxime repellendus porro culpa quae modi, 
+            itaque reiciendis facere repellat corporis, error ad quo aperiam sint?Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Omnis sit saepe ex minima iste maxime repellendus porro culpa quae modi, 
+            itaque reiciendis facere repellat corporis, error ad quo aperiam sint?Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Omnis sit saepe ex minima iste maxime repellendus porro culpa quae modi, 
+            itaque reiciendis facere repellat corporis, error ad quo aperiam sint?<br><br>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Omnis sit saepe ex minima iste maxime repellendus porro culpa quae modi, 
+            itaque reiciendis facere repellat corporis, error ad quo aperiam sint?Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Omnis sit saepe ex minima iste maxime repellendus porro culpa quae modi, 
+            itaque reiciendis facere repellat corporis, error ad quo aperiam sint?Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Omnis sit saepe ex minima iste maxime repellendus porro culpa quae modi, 
+            itaque reiciendis facere repellat corporis, error ad quo aperiam sint?<br><br>
+        </p>
+        <div class="images">
+        <img src="img/agr.png" width="300px" height="300px">
+        <img src="img/agr1.jpg" width="300px" height="300px">
+        <img src="img/agr.png" width="300px" height="300px">
+    </div><br><br>
+    
+    <div class="button">
+    <a href="developers.php">Developers</a>
+</div>
+    </section>
+    <script src="script.js"></script>
+</body>
+</html>
